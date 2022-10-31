053ec420cdc1dcae8af5951e6ed301640e4aba5b
Matheus Catarino <matheus-catarino@hotmail.com>
2022-10-31 15:59:41 -0300

    update to zig stage 2

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
-# Zig PKCS#11 Library
+# Zig PKCS#11 Library [WiP]
 
 ![Build status](https://github.com/kassane/zig-pkcs11/workflows/CI/badge.svg)
 ![GitHub](https://img.shields.io/github/license/kassane/zig-pkcs11?style=flat-square)
 
+
+### Non-functional project in progress yet!
+
+---
 
 ## What is Zig PKCS#11 Library?
 
 
 This is a library which brings support for PKCS#11 v2.40 to Zig.
 
----
+
+
+## Requirements
+
+**Install:**
+
+- [zig v0.10.0 (self-hosting)](https://ziglang/download)
+
+
+**Build library:**
+
+```bash
+zig build -D{Options: release-safe|release-fast|release-small}
+```
+
 
 ## References
 
--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
@@ -7,6 +8,9 @@ pub fn build(b: *std.build.Builder) void {
 
     // Standard target options allow the person running `zig build` to select
     const target = b.standardTargetOptions(.{});
+
+    if (comptime !checkVersion())
+        @compileError("Old compiler!");
 
     // prominent errors are printed to the console, and the build fails.
     b.prominent_compile_errors = true;
@@ -28,4 +32,15 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&tests.step);
+}
+
+fn checkVersion() bool {
+    if (!@hasDecl(builtin, "zig_version")) {
+        return false;
+    }
+
+    const needed_version = std.SemanticVersion.parse("0.10.0-dev.4720") catch unreachable;
+    const version = builtin.zig_version;
+    const order = version.order(needed_version);
+    return order != .lt;
 }
