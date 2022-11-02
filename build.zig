03306e491dbdbcf532b7883f634c77176e94638a
Matheus Catarino <matheus-catarino@hotmail.com>
2022-11-02 14:59:29 -0300

    refactoring: - CK Errors added - CK methods added - (test) call dynlib - clean build.zig - no linking libc

--- build.zig
+++ build.zig
@@ -15,20 +15,13 @@ pub fn build(b: *std.build.Builder) void {
     // prominent errors are printed to the console, and the build fails.
     b.prominent_compile_errors = true;
 
-    var env = std.process.getEnvMap(b.allocator) catch @panic("Env undefined");
     const lib = b.addSharedLibrary("zig-pkcs11", "src/lib.zig", .unversioned);
     lib.setBuildMode(mode);
     lib.setTarget(target);
-    lib.addLibraryPath(env.get("PKCS11_SOFTHSM2_MODULE") orelse "/usr/lib/softhsm");
-    lib.linkSystemLibrary("softhsm2");
-    lib.linkLibC();
     lib.install();
 
     var tests = b.addTest("src/lib.zig");
     tests.setBuildMode(mode);
-    tests.addLibraryPath(env.get("PKCS11_SOFTHSM2_MODULE") orelse "/usr/lib/softhsm");
-    // tests.linkSystemLibrary("softhsm2");
-    tests.linkLibC();
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&tests.step);
--- src/lib.zig
+++ src/lib.zig
@@ -1,12 +1,39 @@
 const std = @import("std");
 const pkcs11 = @import("pkcs11.zig");
 const crypto = std.crypto;
+const mem = std.mem;
 const dyn = std.DynLib;
 const testing = std.testing;
 
-test "Detecting libsoftHSM2" {
-    // embedded lib on compilation time
-    _ = @embedFile("/usr/local/lib/softhsm/libsofthsm2.so");
+pub fn getLibP11Path() []const u8 {
+    return "/usr/lib/softhsm/libsofthsm2.so";
+}
+
+export fn New(module: [*:0]const u8) callconv(.C) ?*pkcs11.Context {
+    var ctx: pkcs11.Context = undefined;
+    var lib = dyn.open(mem.sliceTo(module, 0)) catch |err| {
+        std.log.err("\nError: Library not found!\nCode: {}\n", .{err});
+        return &ctx;
+    };
+    defer lib.close();
+
+    var C_GetInfo = lib.lookup(pkcs11.CK_C_GetInfo, "CK_C_GetInfo");
+    ctx.sym.C_GetInfo = C_GetInfo.?.?;
+    return &ctx;
+}
+
+export fn Finalize(ctx: *pkcs11.Context) callconv(.C) ?*pkcs11.Context {
+    return ctx;
+}
+
+test "Detecting libSoftHSM 2" {
+    // convert []const u8 to [*:0]const u8
+    var libname: [getLibP11Path().len:0]u8 = undefined;
+    mem.copy(u8, libname[0..libname.len], getLibP11Path());
+    libname[getLibP11Path().len] = 0;
+
+    const ctx = New(libname[0..getLibP11Path().len :0]);
+    _ = ctx.?.*.sym.C_GetInfo;
 }
 
 test "Reference all the declarations" {
